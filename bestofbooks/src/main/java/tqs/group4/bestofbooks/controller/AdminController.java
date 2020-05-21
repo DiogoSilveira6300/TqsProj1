9bb98a4b1850b8039d913e17c740a291a5f49004
Diogo Silveira <diogo.silveira10@ua.pt>
2020-05-21 18:12:36 +0100

    ...

--- bestofbooks/src/main/java/tqs/group4/bestofbooks/controller/AdminController.java
+++ bestofbooks/src/main/java/tqs/group4/bestofbooks/controller/AdminController.java
@@ -1,5 +1,5 @@
 package tqs.group4.bestofbooks.controller;
-
+//
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
