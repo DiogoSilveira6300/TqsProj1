8d24fd211e5c984866f8c11f602ef91874b75e0e
Diogo Silveira <diogo.silveira10@ua.pt>
2020-05-22 23:59:13 +0100

    ...

--- bestofbooks/src/main/java/tqs/group4/bestofbooks/BestofbooksApplication.java
+++ bestofbooks/src/main/java/tqs/group4/bestofbooks/BestofbooksApplication.java
@@ -1,5 +1,5 @@
 package tqs.group4.bestofbooks;
-/////////////////
+//////////////////
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
